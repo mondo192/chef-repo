a78b360c73af293431787f7ccefc98716d206a28
mondo192 <31599023+mondo192@users.noreply.github.com>
2020-07-31 16:40:31 +0100

    :tada:

---
+++ cookbooks/apache/recipes/server.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd'
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+service 'httpd' do
+  action [:start, :enable]
+end
